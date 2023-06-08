b406ba08eb2835d9746f17a1176036eb687f79a6
Karim Tawfik <karim.tawfik17@gmail.com>
2023-06-08 19:51:08 +0300

    add showing-count functionality

--- shop.js
+++ shop.js
@@ -2,7 +2,20 @@ import { getProducts } from "./globals.js";
 import renderProducts from "./view.js";
 let startProduct = 0;
 let productPerPage = 9;
-getProducts().then(viewProducts);
-function viewProducts(products) {
-  renderProducts(products, startProduct, productPerPage);
+let viewedProducts = [];
+function reRender() {
+  renderProducts(viewedProducts, startProduct, productPerPage);
 }
+getProducts().then((products) => {
+  viewedProducts = products;
+  reRender();
+});
+
+document.getElementById("count-per-page").onclick = (e) => {
+  e.preventDefault();
+  const count = e.target.dataset.count;
+  if (count) {
+    productPerPage = +count;
+    reRender();
+  }
+};
