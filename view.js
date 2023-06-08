3a74db4dc81132cd09282f869086915bf79eb155
Karim Tawfik <karim.tawfik17@gmail.com>
2023-06-08 19:06:05 +0300

    add product viewing functionality

--- view.js
+++ view.js
@@ -1,18 +1,25 @@
-const parser = new DOMParser();
-getProducts().then(render);
+export default function renderProducts(products, start = 0, count = 9) {
+  const parent = document.getElementById("products");
+  parent.innerHTML = "";
+  for (let i = start; i < start + count; i++) {
+    parent.appendChild(createProductUI(products[i]));
+  }
+}
 
-function renderProduct(product) {
-  let el = parser.parseFromString(
+function createProductUI(product) {
+  const parser = new DOMParser();
+
+  let productUIel = parser.parseFromString(
     `
 <div class="col-lg-4 col-md-6 col-sm-6 pb-1">
   <div class="product-item bg-light mb-4">
     <div class="product-img position-relative overflow-hidden">
-      <img class="img-fluid w-100" src="img/product-1.jpg" alt="" />
+      <img class="img-fluid w-100" src="${product.image}" alt="" />
       <div class="product-action">
-        <a class="btn btn-outline-dark btn-square" href="">
+        <a data-action='cart' class="btn btn-outline-dark btn-square" >
           <i class="fa fa-shopping-cart"></i>
         </a>
-        <a class="btn btn-outline-dark btn-square" href="">
+        <a data-action='heart' class="btn btn-outline-dark btn-square" href="">
           <i class="far fa-heart"></i>
         </a>
         <a class="btn btn-outline-dark btn-square" href="">
@@ -25,21 +32,20 @@ function renderProduct(product) {
     </div>
     <div class="text-center py-4">
       <a class="h6 text-decoration-none text-truncate" href="">
-        Product Name Goes Here
+        ${product.name}
       </a>
       <div class="d-flex align-items-center justify-content-center mt-2">
-        <h5>$123.00</h5>
+        <h5>$${(product.price - product.price * product.discount).toFixed(
+          2
+        )}</h5>
         <h6 class="text-muted ml-2">
-          <del>$123.00</del>
+          <del>$${product.price.toFixed(2)}
+          </del>
         </h6>
       </div>
       <div class="d-flex align-items-center justify-content-center mb-1">
-        <small class="fa fa-star text-primary mr-1"></small>
-        <small class="fa fa-star text-primary mr-1"></small>
-        <small class="fa fa-star text-primary mr-1"></small>
-        <small class="fa fa-star text-primary mr-1"></small>
-        <small class="fa fa-star text-primary mr-1"></small>
-        <small>(99)</small>
+          ${renderRatings(product.rating)}
+        <small>(${product.rating_count})</small>
       </div>
     </div>
   </div>
@@ -47,18 +53,64 @@ function renderProduct(product) {
   `,
     "text/html"
   );
-  el = el.body.firstChild;
-  return el;
+  productUIel = productUIel.body.firstChild;
+  addHandersToProductUI(product, productUIel);
+  return productUIel;
 }
-// window.renderProduct = renderProduct;
-function render(products) {
-  const parent = document.getElementById("products");
-  for (let i = 0; i < 12; i++) {
-    const item = renderProduct();
-    console.log(item);
-    // parent.insertAdjacentHTML("beforeend", renderProduct(products[1]));
-    // parent.insertAdjacentHTML("beforeend", renderProduct(products[1]));
-    // parent.insertAdjacentHTML("beforeend", renderProduct(products[1]));
-    parent.appendChild(item);
+
+function addHandersToProductUI(product, productUIel) {
+  const cartBtn = productUIel.querySelector('[data-action="cart"');
+  const heartBtn = productUIel.querySelector('[data-action="heart"');
+  cartBtn.onclick = (e) => {
+    e.preventDefault();
+    console.log(product._id, "added to cart"); // add to cart logic here
+  };
+  heartBtn.onclick = (e) => {
+    e.preventDefault();
+    console.log(product._id, "added to favorites"); // favorite logic here
+  };
+}
+
+function renderRatings(rating) {
+  if (rating < 0) {
+    console.warn("invalid rating : ", rating);
+    rating = 0;
   }
+  if (rating > 5) {
+    console.warn("invalid rating : ", rating);
+    rating = 5;
+  }
+  let solid = rating;
+  let empty = 5 - rating;
+  let half = 0;
+  if (isFloat(rating)) {
+    solid = Math.floor(rating);
+    empty = 4 - solid;
+    half = 1;
+  }
+  let ratingSection = ``;
+  ratingSection += renderRatingStarsFull(solid);
+  if (half) {
+    ratingSection += `<small class="fa fa-star text-primary mr-1"></small>`; // to be edited to half rating
+  }
+  ratingSection += renderRatingStarsEmpty(empty);
+  return ratingSection;
+}
+
+function renderRatingStarsFull(n) {
+  let stars = ``;
+  for (let i = 0; i < n; i++) {
+    stars += `<small class="fa fa-star text-primary mr-1"></small>`;
+  }
+  return stars;
+}
+function renderRatingStarsEmpty(n) {
+  let stars = ``;
+  for (let i = 0; i < n; i++) {
+    stars += `<small class="far fa-star text-primary mr-1"></small>`;
+  }
+  return stars;
+}
+function isFloat(n) {
+  return n % 1 !== 0;
 }
