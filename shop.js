95b87ad95d516639eca152a64d22db973ce8e5d1
Karim Tawfik <karim.tawfik17@gmail.com>
2023-06-08 22:00:31 +0300

    add filtering products functionality

--- shop.js
+++ shop.js
@@ -4,11 +4,23 @@ let startProduct = 0;
 let productPerPage = 9;
 let allProducts = [];
 let viewedProducts = [];
+function cleanData(products) {
+  const randomColor = () =>
+    ["black", "white", "red", "blue", "green"][Math.floor(Math.random() * 5)];
+  const randomSize = () =>
+    ["XS", "S", "M", "L", "XL"][Math.floor(Math.random() * 5)];
+  return products.map((product) => ({
+    ...product,
+    color: randomColor(),
+    size: randomSize(),
+  }));
+}
 function reRender() {
-  //   console.log(viewedProducts[0]);
+  console.log(viewedProducts[0]);
   renderProducts(viewedProducts, startProduct, productPerPage);
 }
 getProducts().then((products) => {
+  products = cleanData(products);
   allProducts = products;
   viewedProducts = products;
   reRender();
@@ -42,6 +54,44 @@ document.getElementById("sort-products").onclick = (e) => {
       throw new Error("Unexpected sorting type : ", sortType);
   }
 
-  viewedProducts = [...allProducts].sort(sortFn);
+  viewedProducts = [...viewedProducts].sort(sortFn);
   reRender();
 };
+
+const priceFilterForm = document.getElementById("price-filter");
+priceFilterForm.onchange = filterProducts;
+const colorFilterForm = document.getElementById("color-filter");
+colorFilterForm.onchange = filterProducts;
+const sizeFilterForm = document.getElementById("size-filter");
+sizeFilterForm.onchange = filterProducts;
+
+function filterProductByPrice(product, shownPriceRanges) {
+  if (shownPriceRanges.includes("all")) return true;
+  for (let range of shownPriceRanges) {
+    range = +range;
+    if (product.price >= range && product.price < range + 100) {
+      return true;
+    }
+  }
+  return false;
+}
+function filterProductByColor(product, shownColors) {
+  return shownColors.includes("all") || shownColors.includes(product.color);
+}
+function filterProductBySize(product, shownSizes) {
+  return shownSizes.includes("all") || shownSizes.includes(product.size);
+}
+
+function filterProducts() {
+  const shownPriceRanges = [...new FormData(priceFilterForm).keys()];
+  const shownColors = [...new FormData(colorFilterForm).keys()];
+  const shownSizes = [...new FormData(sizeFilterForm).keys()];
+  viewedProducts = allProducts.filter(
+    (product) =>
+      filterProductByPrice(product, shownPriceRanges) &&
+      filterProductByColor(product, shownColors) &&
+      filterProductBySize(product, shownSizes)
+  );
+  reRender();
+  // console.log(shownPriceRanges, shownColors, shownSizes);
+}
