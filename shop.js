1ab4c4a367cb2dba65c17906cbf332669cd52948
Karim Tawfik <karim.tawfik17@gmail.com>
2023-06-08 19:20:32 +0300

    view products on page

--- shop.js
+++ shop.js
@@ -1,2 +1,8 @@
 import { getProducts } from "./globals.js";
-getProducts().then(console.log);
+import renderProducts from "./view.js";
+let startProduct = 0;
+let productPerPage = 9;
+getProducts().then(viewProducts);
+function viewProducts(products) {
+  renderProducts(products, startProduct, productPerPage);
+}
