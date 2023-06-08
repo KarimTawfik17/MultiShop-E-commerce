29ff8002620d5cc9b8fa513d265aae051cdb364c
Karim Tawfik <karim.tawfik17@gmail.com>
2023-06-08 20:05:04 +0300

    add sorting functionality

--- shop.js
+++ shop.js
@@ -4,6 +4,7 @@ let startProduct = 0;
 let productPerPage = 9;
 let viewedProducts = [];
 function reRender() {
+  //   console.log(viewedProducts[0]);
   renderProducts(viewedProducts, startProduct, productPerPage);
 }
 getProducts().then((products) => {
@@ -18,4 +19,26 @@ document.getElementById("count-per-page").onclick = (e) => {
     productPerPage = +count;
     reRender();
   }
+};
+document.getElementById("sort-products").onclick = (e) => {
+  e.preventDefault();
+  const sortType = e.target.dataset.sort;
+  if (!sortType) return;
+  let sortFn;
+  switch (sortType) {
+    case "price":
+      sortFn = (p1, p2) => p1.price - p2.price;
+      break;
+    case "popularity":
+      sortFn = (p1, p2) => p1.rating_count - p2.rating_count;
+      break;
+    case "rating":
+      sortFn = (p1, p2) => p1.rating - p2.rating;
+      break;
+
+    default:
+      throw new Error("Unexpected sorting type : ", sortType);
+  }
+  viewedProducts = viewedProducts.sort(sortFn);
+  reRender();
 };
